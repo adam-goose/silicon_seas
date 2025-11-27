3b228b596b8718a5c75158eb8af322bedd657672
Adam Ahmed-Keyte <78147202+adam-a-k@users.noreply.github.com>
2025-11-27 21:30:45 +0000

    Added 20 opposing companies, 50 simulations
    Added 10 scheduler companies and 10 arch enemy companies. Added a loop for the simulation to run 50 times (based on real test environment). Also starting to implement same income algorithm as Mable metrics overview into the metrics reader that Adam created.

---
+++ main_competition_playground_modified.py
@@ -0,0 +1,26 @@
+from mable.examples import environment, fleets, companies
+
+import groupn
+
+
+def build_specification():
+    number_of_month = 12
+    trades_per_auction = 5
+    
+    specifications_builder = environment.get_specification_builder(trades_per_occurrence=trades_per_auction,num_auctions=number_of_month)
+    my_fleet = fleets.mixed_fleet(num_suezmax=1, num_aframax=1, num_vlcc=1)
+    specifications_builder.add_company(groupn.CompanyZ6.Data(groupn.CompanyZ6, my_fleet, groupn.CompanyZ6.__name__))
+
+    for company in range(10):
+        fleet = fleets.mixed_fleet(num_suezmax=1, num_aframax=1, num_vlcc=1)
+        specifications_builder.add_company(companies.MyArchEnemy.Data(companies.MyArchEnemy, fleet, f"Arch Enemy Ltd.{company}",profit_factor=1.5))
+    
+        the_scheduler_fleet = fleets.mixed_fleet(num_suezmax=1, num_aframax=1, num_vlcc=1)
+        specifications_builder.add_company(companies.TheScheduler.Data(companies.TheScheduler, the_scheduler_fleet, f"The Scheduler LP {company}",profit_factor=1.4))
+        
+    sim = environment.generate_simulation(specifications_builder,show_detailed_auction_outcome=True,global_agent_timeout=60)
+    sim.run()
+
+if __name__ == '__main__':
+
+    [build_specification() for i in range(50)]
--- metrics_comp_reader.py
+++ metrics_comp_reader.py
@@ -1,19 +1,47 @@
 import os
 import json
 import matplotlib.pyplot as plt
+from mable import cli
+import numpy as np
 
-os.chdir(r"B:\Google Drive Sync\UoS MSc Artificial Intelligence\Intelligent Agents\Labs\Lab 3")
-print("Current working directory:", os.getcwd())
+os.chdir(os.getcwd())
+
+def profit(metrics_file_name):
+
+    #print(f"Overview for {metrics_file_name}.")
+    with open(metrics_file_name, "r") as f:
+        metrics = json.load(f)
+    for one_company_key in metrics["company_metrics"]:
+        company_name = metrics["company_names"][one_company_key]
+        print(f"{company_name}")
+        cost = 0
+        if "fuel_cost" in metrics["company_metrics"][one_company_key]:
+            cost = metrics["company_metrics"][one_company_key]["fuel_cost"]
+        penalty = metrics["global_metrics"]["penalty"][one_company_key]
+        revenue = 0
+        all_outcomes = metrics["global_metrics"]["auction_outcomes"]
+        all_outcomes_company_per_round = [d[one_company_key] for d in all_outcomes if one_company_key in d]
+        all_outcomes_company = [x for sublist in all_outcomes_company_per_round for x in sublist]
+        all_payments = [d["payment"] for d in all_outcomes_company]
+        revenue += sum(all_payments)
+        income = revenue - cost - penalty
+        print(income)
+        #return income
+
 
 files = [f for f in os.listdir()
          if f.startswith("metrics_competition") and f.endswith(".json")]
+
 
 plt.figure()
 labels = []
 
 for filename in files:
+    profit(filename)
     with open(filename) as f:
         data = json.load(f)
+    # print(dict(filename))
+    # cli.task_metrics_overview(dict(filename))
 
     window_sums = []      # payment per auction window
     total_payment = 0
@@ -37,8 +65,7 @@ for filename in files:
         # if company 0 had no trades this window, window_total = 0
         window_sums.append(window_total)
 
-    print(f"{filename} => payment={total_payment:.2f}, "
-          f"fulfilled={fulfilled}, unfulfilled={unfulfilled}")
+    #print(f"{filename} => payment={total_payment:.2f}, " f"fulfilled={fulfilled}, unfulfilled={unfulfilled}")
 
     # cumulative over windows
     cumulative = []
@@ -56,4 +83,4 @@ plt.ylabel("Cumulative Payment")
 plt.grid(True)
 plt.legend(labels)
 plt.tight_layout()
-plt.show()
+#plt.show()
